7cebdd3991558fadae596875091e08b2e23c9968
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-08-20 12:31:43 -0300

    part1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,12 +6,18 @@ const Statistics = (props) => {
   return(
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.good + props.neutral + props.bad}</p>
-      <p>average {(props.good + props.neutral + props.bad) > 0 ? (props.good - props.bad) / (props.good + props.neutral + props.bad) : 0}%</p>
-      <p>positive {(props.good + props.neutral + props.bad) > 0 ? props.good / (props.good + props.neutral + props.bad) * 100 : 0}%</p>
+      {(props.good + props.neutral + props.bad) > 0 ? (
+        <div>
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {props.good + props.neutral + props.bad}</p>
+          <p>average {(props.good + props.neutral + props.bad) > 0 ? (props.good - props.bad) / (props.good + props.neutral + props.bad) : 0}%</p>
+          <p>positive {(props.good + props.neutral + props.bad) > 0 ? props.good / (props.good + props.neutral + props.bad) * 100 : 0}%</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
 }
