a7eae625584f1c84d796b0fffc5ab23b599ddb2d
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-08-20 12:47:51 -0300

    part1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,14 +7,16 @@ const Statistics = (props) => {
     <div>
       <h1>statistics</h1>
       {(props.good + props.neutral + props.bad) > 0 ? (
-        <div>
-          <Statistic text="good" value={props.good} />
-          <Statistic text="neutral" value={props.neutral} />
-          <Statistic text="bad" value={props.bad} />
-          <Statistic text="all" value={props.good + props.neutral + props.bad} />
-          <Statistic text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-          <Statistic text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
-        </div>
+        <table>
+          <tbody>
+            <Statistic text="good" value={props.good} />
+            <Statistic text="neutral" value={props.neutral} />
+            <Statistic text="bad" value={props.bad} />
+            <Statistic text="all" value={props.good + props.neutral + props.bad} />
+            <Statistic text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+            <Statistic text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
@@ -24,7 +26,10 @@ const Statistics = (props) => {
 
 const Statistic = (props) => {
   return(
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
