0c76b10e6455971b34889857f40806f1439b91f3
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-08-25 19:32:42 -0300

    part 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const CountryDetails = ({ country }) => {
+  const [show, setShow] = useState(false)
+
+  return(
+    <div>
+      {show ? (
+        <div>
+          <h1>{country.name}</h1>
+          <button onClick={() => setShow(!show)}>hide</button>
+          <p>capital {country.capital}</p>
+          <p>population {country.population}</p>
+          <h2>languages</h2>
+          <ul>
+            {country.languages.map((language, i) => (
+              <li key={i}>{language.name}</li>
+            ))}
+          </ul>
+          <img src={country.flag} alt={country.name} />  
+        </div>
+      ) : (
+        <div>
+          <span>{country.name}  </span>
+          <button onClick={() => setShow(!show)}>show</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
@@ -20,7 +49,7 @@ const App = () => {
           <p>Too many matches, specify another filter</p>
         ) : filteredCountries.length === 1 ? (
           filteredCountries.map(country => (
-            <div key={country.cioc + country.name}>
+            <div key={country.cioc + country.name} country={country}>
               <h1>{country.name}</h1>
               <p>capital {country.capital}</p>
               <p>population {country.population}</p>
@@ -30,12 +59,12 @@ const App = () => {
                   <li key={i}>{language.name}</li>
                 ))}
               </ul>
-              <img src={country.flag} alt={country.name} />
+              <img src={country.flag} alt={country.name} />  
             </div>
           ))
-        ) : filteredCountries.map(country => (
-          <p key={country.cioc + country.name}>{country.name}</p>
-        ))}
+        ) : filteredCountries.map(country => 
+            <CountryDetails key={country.cioc + country.name} country={country} />
+        )}
       </div>
     </div>
   )
